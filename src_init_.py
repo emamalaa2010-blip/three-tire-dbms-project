a23405a4996f8a8a439f548fbb37829ba3612dd9
Alaa <emamalaa2010@gmail.com>
2025-12-28 13:57:07 -0500

    Update src_init_.py
    Signed-off-by: Alaa <emamalaa2010@gmail.com>

--- src_init_.py
+++ src_init_.py
@@ -1,1 +1,12 @@
+from data.mysql_db import connect_mysql
+
+def save_excel_data():
+    connection = connect_mysql()
+
+    if connection:
+        print("Ready to save Excel data to MySQL")
+        connection.close()
+    else:
+        print("MySQL connection failed")
+
 
