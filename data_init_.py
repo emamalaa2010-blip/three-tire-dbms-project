02fce72105c51487c5bded37689c919ed97563c5
Alaa <emamalaa2010@gmail.com>
2025-12-28 13:55:33 -0500

    Update data_init_.py
    Signed-off-by: Alaa <emamalaa2010@gmail.com>

--- data_init_.py
+++ data_init_.py
@@ -1,1 +1,20 @@
+import mysql.connector
+
+def connect_mysql():
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="password",
+            database="school_db"
+        )
+
+        if connection.is_connected():
+            print("MySQL connected successfully")
+            return connection
+
+    except mysql.connector.Error as e:
+        print("Error connecting to MySQL:", e)
+        return None
+
 
